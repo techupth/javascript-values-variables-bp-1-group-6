429541c8e19839566d160245c1cb768a0a1a41f0
Marisa Phungbunphra <memee.sks@gmail.com>
2023-09-06 19:37:47 +0700

    feat: add assignment from mie

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const net = 15000;
+const openingRestaurant = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+
+console.log(address);
