ffc6f144e3ad0dbf71ac56680b3e10aa8cb79e5f
Natkamon Saelim <pim-i-smart@hotmail.com>
2023-09-16 19:18:32 +0700

    feat: add assignment from pim

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const billTotal = 15000;
+const totalRestaurants = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+billTotal = 5000;
+totalRestaurants = 2500;
