d0fbf82d0ac02b55523cb321e2ce6812d13f71dc
Kantima <k.chooarwut@gmail.com>
2023-09-16 22:45:45 +0700

    feat: add assignment from earth

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 BangKruay -Sainoi Bang Kruay Bang Kruai0";
+const resterance = "Chef's Table by Chef James";
+const sell = 15000;
+const storeBranch = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 Country Rd #4Meeker, Colorado(CO) 81641";
+sell = 5000;
+storeBranch = 2500;
