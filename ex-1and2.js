86a71af4913106833168aa33a8cfb1c55dec544a
Natkamon <natkamonsaelim@gmail.com>
2023-12-05 18:04:38 +0700

    feat: new ex1 and 2 from pim

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,14 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
-let name = "Karter";
+let firstName = "Karter";
 let lastName = "Anderson";
-let address = "54/24 Gp 5 BangKruay -Sainoi Bang Kruay Bang Kruai0";
-const resterance = "Chef's Table by Chef James";
-const sell = 15000;
-const storeBranch = 1500;
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = "15000";
+const restaurantNumber = "1500";
 
 //Exercise #2: Variable Reassignment
 //Start coding here
-address = "(970) 878-53292400 Country Rd #4Meeker, Colorado(CO) 81641";
-sell = 5000;
-storeBranch = 2500;
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+restaurantNumber = 2500;
